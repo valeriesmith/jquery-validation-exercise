6b6ae6df7243813f4d18295e9ada1dc4b352ffec
Valerie Smith <valeriesmithdbc@gmail.com>
2015-06-29 16:41:30 -0700

    adds tag feature

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   	resources :comments
   end
 
+  resources :tags
+
 
 
 
--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629195007) do
+ActiveRecord::Schema.define(version: 20150629212028) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author_name"
@@ -22,6 +22,22 @@ ActiveRecord::Schema.define(version: 20150629195007) do
   end
 
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+  add_index "taggings", ["user_id"], name: "index_taggings_on_user_id"
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
