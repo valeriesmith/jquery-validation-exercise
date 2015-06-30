06ce995dfd494a4825d2343a7d1f3549c99c9ffc
Valerie Smith <valeriesmithdbc@gmail.com>
2015-06-29 18:04:52 -0700

    adds paperclip gem

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629212028) do
+ActiveRecord::Schema.define(version: 20150630002713) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author_name"
@@ -45,8 +45,12 @@ ActiveRecord::Schema.define(version: 20150629212028) do
     t.text     "country"
     t.text     "zipcode"
     t.text     "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
