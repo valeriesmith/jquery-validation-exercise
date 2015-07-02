543a12422501e41e69627b247d88fc9e9887b8a6
Valerie Smith <valeriesmithdbc@gmail.com>
2015-07-02 10:36:08 -0700

    experiments with validations

--- blogger/app/assets/javascripts/application.js
+++ blogger/app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+// = require jquery
+// = require jquery_ujs
+// = require jquery.validate.min.js
+// = require turbolinks
+// = require_tree .
+
+// = require validate.js
